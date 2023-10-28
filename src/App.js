dc6a06bd881a495d90f5e1cd5f4cf7405cfabb8f
sayakATPL <sayak.sengupta@atpl.live>
2023-10-28 23:35:17 +0530

    code transfer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import UserType from './pages/UserType';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import About from './pages/About';
+import Advisor from './pages/Advisor';
+import ContactUs from './pages/ContactUs';
+import Blog from './pages/Blog';
+import Faq from './pages/Faq';
+import AddBlog from './pages/AddBlog';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router> 
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/about" exact element={<About />} />
+            <Route path="/advisor" exact element={<Advisor />} />
+            <Route path="/user-selection" exact element={<UserType />} />
+            <Route path="/login" exact element={<Login />} />
+            <Route path="/dashboard" exact element={<Dashboard />} />
+            <Route path="/contact-us" exact element={<ContactUs />} />
+            <Route path="/blog" exact element={<Blog />} />
+            <Route path="/add-blog" exact element={<AddBlog />} />
+            <Route path="/faq" exact element={<Faq />} />
+          </Routes>
+      </Router> 
   );
 }
+
 
 export default App;
